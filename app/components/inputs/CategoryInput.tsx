a7052303e07514e8bedcdfa3e6908f5fd104d7e0
Kelvinmaganigani <kelvinmaganigani800@gmail.com>
2024-09-05 22:38:21 +0300

    category selection color

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -11,7 +11,7 @@ const CategoryInput = ({selected, label, icon: Icon, onclick}: CategoryInputProp
   return (
     <div onClick={() => onclick(label)} 
         className={`rounded-xl border-2 p-4 flex flex-col items-center gap-2 hover:border-slate-500 transition cursor-pointer
-            ${selected? 'border-slate-500' : 'border-slate-200'}
+            ${selected? 'border-teal-400' : 'border-slate-200'}
             `}
     >
         <Icon size={30} />
